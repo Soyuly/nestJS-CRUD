8bb6d6e6fe4e7fe26a3bc0910487e50101638e30
Soyuly <thduf0623@likelion.org>
2022-02-08 02:12:38 +0900

    create

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { Board } from './board.model';
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid';
+
 @Injectable()
 export class BoardsService {
   private boards: Board[] = [];
@@ -7,5 +9,18 @@ export class BoardsService {
   //Read
   getAllBoards(): Board[] {
     return this.boards;
+  }
+
+  //Create
+  createBoard(title: string, description: string) {
+    const board: Board = {
+      id: uuid(),
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    };
+
+    this.boards.push(board);
+    return board;
   }
 }
